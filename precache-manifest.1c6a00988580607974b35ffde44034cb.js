50f489e822728611eb6b5233af16d3ec96630e18
ZeroDawn0D <umang26112003@gmail.com>
2020-08-15 15:32:08 +0530

    added IOL

---
+++ precache-manifest.1c6a00988580607974b35ffde44034cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c50a150b2b1e947488a8c6d867841e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b5c99cf84410cf0c8b60",
+    "url": "./static/js/2.cb031749.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.cb031749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7d4ee04bea0d96206c8",
+    "url": "./static/js/main.255aca96.chunk.js"
+  },
+  {
+    "revision": "f3b5d637f7c443d8c44f",
+    "url": "./static/js/runtime-main.a7047017.js"
+  }
+]);
